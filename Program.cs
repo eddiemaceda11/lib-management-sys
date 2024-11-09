7fe1ccb8c80353545dd0f1ed382f62bf04215ce1
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-09 16:12:45 -0700

    Install Spectre.Console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
+
+var choice = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+        .Title("What do you want to do next?")
+        .AddChoices(menuChoices));
+        */
+
+using Spectre.Console;
+
+var menuChoices = new string[3] {"View Books", "Add Book", "Delete Book"};
+
+var choice = AnsiConsole.Prompt(
+  new SelectionPrompt<string>()
+  .Title("What next?")
+  .AddChoices(menuChoices)
+);
