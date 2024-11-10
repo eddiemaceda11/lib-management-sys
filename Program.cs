a88297790ecedeed97b3e683f45962efcd775087
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-10 11:13:06 -0700

    Extract functionality from the switch cases into separate functions

--- Program.cs
+++ Program.cs
@@ -21,77 +21,91 @@ while(true)
 
   switch(choice)
   {
-    case MenuOption.ViewBooks;
-      // Spectre's MarkupLine method is useful for styling strings.
-      // We'll use it as a standard do print messages to the console.
-      AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
-
-      // Printing each book to the console with a loop
-      foreach (var book in books)
-      {
-        AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
-      }
-
-      // Having the user press a key before continuing, or the menu
-      // wouldn't be visualisable  
-      AnsiConsole.MarkupLine("Pres any key to continue.");
-      Console.ReadKey();
+    case MenuOption.ViewBooks:
+      ViewBooks();
       break;
 
     case MenuOption.AddBook:
-    // Spectre's Ask<> method allows us to prompt a message to grab 
-    // the user's input. We can pass the type we expect as an answer
-    // for validation. We're storing the answer in the 'title' variable
-      var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
-
-      // Checking if the book already exists to avoid duplication
-      if (books.Contains(title))
-      {
-        AnsiConsole.MarkupLine("[red]This book already exists[/]");
-      }
-      else
-      {
-        // If book doesn't exist, add to the list of books
-        books.Add(title);
-        AnsiConsole.MarkupLine("[green]Book added successfully[/]");
-      }
-
-      AnsiConsole.MarkupLine("Press any key to continue.");
-      Console.ReadKey();
+      AddBook();
       break;
 
-    case MenuOption.DeleteBook;
-      // Checking if there are any books to delete and letting the user know
-      if (books.Count == 0)
-      {
-        AnsiConsole.MarkupLine("[red]No books available to delete.[/]");
-        Console.ReadKey();
-        return;
-      }
-
-      /* showing a list of books and letting the user choose with arrows 
-      using SelectionPrompt, similarly to what we do with the menu */
-      var bookToDelete = AnsiConsole.Prompt(
-        new SelectionPrompt<string>()
-        .Title("Select a [red]book[/] to delete:")
-        .AddChoices(books)
-      );
-
-      /* Using the Remove method to delete a book. This method returns a boolean
-      that we can use to present a message in case of success or failure.*/
-      if (books.Remove(bookToDelete))
-      {
-        AnsiConsole.MarkupLine("[red]Book deleted successfully![/]");
-      }
-      else
-      {
-        AnsiConsole.MarkupLine("[red]Book not found.[/]"); 
-      }
-
-      AnsiConsole.MarkupLine("Press Any Key to Continue.");
-      Console.ReadKey();
+    case MenuOption.DeleteBook:
+      DeleteBook();
       break;
   }
+}
+
+void ViewBooks()
+{
+   // Spectre's MarkupLine method is useful for styling strings.
+   // We'll use it as a standard do print messages to the console.
+    AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
+
+    // Printing each book to the console with a loop
+    foreach (var book in books)
+    {
+      AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
+    }
+
+    // Having the user press a key before continuing, or the menu
+    // wouldn't be visualisable  
+    AnsiConsole.MarkupLine("Pres any key to continue.");
+    Console.ReadKey();
+}
+
+void AddBook() 
+{
+  // the user's input. We can pass the type we expect as an answer
+  // for validation. We're storing the answer in the 'title' variable
+  var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
+
+  // checking if the book already exists to avoid duplication
+  if (books.Contains(title))
+  {
+    AnsiConsole.MarkupLine("[red]This book already exists[/]");
+  }
+  else 
+  {
+    // If book doesn't exist, add to the list of books
+    books.Add(title);
+    AnsiConsole.MarkupLine("[green]Book added successfully[/]");
+  }
+
+  AnsiConsole.MarkupLine("Press any ket to continue");
+  Console.ReadKey();
+}
+
+void DeleteBook()
+{
+  // Check to see if there are any books to delete and let the user know
+    if(books.Count == 0)
+    {
+      AnsiConsole.MarkupLine("[red]No books available to delete.[/]");
+      Console.ReadKey();
+      return;
+    }
+
+     /* showing a list of books and letting the user choose with arrows 
+      using SelectionPrompt, similarly to what we do with the menu */
+    var bookToDelete = AnsiConsole.Prompt(
+      new SelectionPrompt<string>()
+      .Title("Select a [red]book[/] to delete")
+      .AddChoices(books)
+    );
+
+     /* Using the Remove method to delete a book. This method returns a boolean
+      that we can use to present a message in case of success or failure.*/
+    if(books.Remove(bookToDelete))
+    {
+      AnsiConsole.MarkupLine("[red]Book deleted successfully![/]");
+    }
+    else
+    {
+      AnsiConsole.MarkupLine("[red]Book not found.[/]"); 
+    }
+
+    AnsiConsole.MarkupLine("Press Any Key to Continue.");
+    Console.ReadKey();
 }
 
 enum MenuOption
