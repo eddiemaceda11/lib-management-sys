6a36adb056896895a5c1504c63d2ac5ac728c7f0
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-11 20:39:38 -0700

    Create BooksController class to hold the functionality for our Library system

---
+++ BooksController.cs
@@ -0,0 +1,82 @@
+
+namespace TCSA.OOP.LibraryManagementSystem
+{
+    internal static class BooksController
+    {
+
+        private static List<string> books = new();
+
+        internal static void ViewBooks()
+        {
+            // Spectre's MarkupLine method is useful for styling strings.
+            // We'll use it as a standard do print messages to the console.
+            AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
+
+            // Printing each book to the console with a loop
+            foreach (var book in books)
+            {
+                AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
+            }
+
+            // Having the user press a key before continuing, or the menu
+            // wouldn't be visualisable  
+            AnsiConsole.MarkupLine("Pres any key to continue.");
+            Console.ReadKey();
+        }
+
+        internal static void AddBook()
+        {
+            // the user's input. We can pass the type we expect as an answer
+            // for validation. We're storing the answer in the 'title' variable
+            var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
+
+            // checking if the book already exists to avoid duplication
+            if (books.Contains(title))
+            {
+                AnsiConsole.MarkupLine("[red]This book already exists[/]");
+            }
+            else
+            {
+                // If book doesn't exist, add to the list of books
+                books.Add(title);
+                AnsiConsole.MarkupLine("[green]Book added successfully[/]");
+            }
+
+            AnsiConsole.MarkupLine("Press any ket to continue");
+            Console.ReadKey();
+        }
+
+        internal static void DeleteBook()
+        {
+            // Check to see if there are any books to delete and let the user know
+            if (books.Count == 0)
+            {
+                AnsiConsole.MarkupLine("[red]No books available to delete.[/]");
+                Console.ReadKey();
+                return;
+            }
+
+            /* showing a list of books and letting the user choose with arrows 
+             using SelectionPrompt, similarly to what we do with the menu */
+            var bookToDelete = AnsiConsole.Prompt(
+              new SelectionPrompt<string>()
+              .Title("Select a [red]book[/] to delete")
+              .AddChoices(books)
+            );
+
+            /* Using the Remove method to delete a book. This method returns a boolean
+             that we can use to present a message in case of success or failure.*/
+            if (books.Remove(bookToDelete))
+            {
+                AnsiConsole.MarkupLine("[red]Book deleted successfully![/]");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Book not found.[/]");
+            }
+
+            AnsiConsole.MarkupLine("Press Any Key to Continue.");
+            Console.ReadKey();
+        }
+    }
+}
--- obj/Debug/net8.0/TCSA.OOP.LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/TCSA.OOP.LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe1ccb8c80353545dd0f1ed382f62bf04215ce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88297790ecedeed97b3e683f45962efcd775087")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
