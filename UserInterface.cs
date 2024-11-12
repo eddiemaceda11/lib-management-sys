7effcf90f6347d2b0fd4e8fc987987fde13bedd9
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-11 21:05:45 -0700

    Add the MainMenu functionality

--- UserInterface.cs
+++ UserInterface.cs
@@ -1,12 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Spectre.Console;
+using static TCSA.OOP.LibraryManagementSystem.Enums;
 
 namespace TCSA.OOP.LibraryManagementSystem
 {
     public class UserInterface
     {
-        
+        internal static void MainMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuOption>()
+                .Title("What menu item next?")
+                .AddChoices(Enum.GetValues<MenuOption>())
+                );
+
+
+
+                switch (choice)
+                {
+                    case MenuOption.ViewBooks:
+                        BooksController.ViewBooks();
+                        break;
+
+                    case MenuOption.AddBook:
+                        BooksController.AddBook();
+                        break;
+
+                    case MenuOption.DeleteBook:
+                        BooksController.DeleteBook();
+                        break;
+                }
+            }
+        }
     }
 }
