355ba35d941a553a1f4be6a207d36095bef0aad5
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-11 21:03:33 -0700

    Remove all code from Program, other than the UserInterface.MainMenu method

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Spectre.Console;
 using TCSA.OOP.LibraryManagementSystem;
 
+UserInterface.MainMenu();
+
 // var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
 
 // var books = new List<string>()
@@ -8,38 +10,5 @@ using TCSA.OOP.LibraryManagementSystem;
 //     "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", "The Hobbit", "Moby-Dick", "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm", "Brave New World", "The Chronicles of Narnia", "The Diary of a Young Girl", "The Alchemist", "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
 // };
 
-while (true)
-{
-  Console.Clear();
 
-  var choice = AnsiConsole.Prompt(
-  new SelectionPrompt<MenuOption>()
-  .Title("What menu item next?")
-  .AddChoices(Enum.GetValues<MenuOption>())
-  );
-
-
-
-  switch (choice)
-  {
-    case MenuOption.ViewBooks:
-      BooksController.ViewBooks();
-      break;
-
-    case MenuOption.AddBook:
-      BooksController.AddBook();
-      break;
-
-    case MenuOption.DeleteBook:
-      BooksController.DeleteBook();
-      break;
-  }
-}
-
-enum MenuOption
-{
-  ViewBooks,
-  AddBook,
-  DeleteBook
-}
 
