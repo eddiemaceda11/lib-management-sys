b1efac3bfe9dbabc3c04ff2b700001ceb0750ad1
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-11 20:47:24 -0700

    Create a UserInterface class/file that will contain the main menu for the program

--- obj/Debug/net8.0/TCSA.OOP.LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/TCSA.OOP.LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88297790ecedeed97b3e683f45962efcd775087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a36adb056896895a5c1504c63d2ac5ac728c7f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
