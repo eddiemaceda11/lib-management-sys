ba5f5b959b33e0f16067ea9ad9b2ee99ee77ed5b
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-11 21:06:25 -0700

    Files updated

--- obj/Debug/net8.0/TCSA.OOP.LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/TCSA.OOP.LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a36adb056896895a5c1504c63d2ac5ac728c7f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40caec5285c38d7c351dbf0408709a738dba806a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCSA.OOP.LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
